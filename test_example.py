0cd8c36a5d335017f8d00f4adde090b45778b7bc
Niket Agrawal <15953349+niketagrawal@users.noreply.github.com>
2023-11-08 22:16:51 +0100

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -2,12 +2,12 @@
 
 import os
 import sys
+
 def add_lists(list1, list2):
     list3 = []
     for i in range(len(list1)):
         list3.append(list1[i] + list2[i])
     return list3
-
 def test_add_lists():
     list1 = [1, 2, 3]
     list2 = [4, 5, 6]
